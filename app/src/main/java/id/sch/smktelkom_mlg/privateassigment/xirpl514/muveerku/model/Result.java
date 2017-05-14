303258a8b80fd725db9ede023fb946e71dc52845
irvinafiradila <irvinafiradila74@gmail.com>
2017-05-14 14:07:51 +0700

    Navigation NowPlaying TopRated ComingSoon

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassigment/xirpl514/muveerku/model/Result.java
@@ -0,0 +1,22 @@
+package id.sch.smktelkom_mlg.privateassigment.xirpl514.muveerku.model;
+
+/**
+ * Created by hyuam on 18/04/2017.
+ */
+
+public class Result {
+
+    public String poster_path;
+    public Boolean adult;
+    public String overview;
+    public String release_date;
+    public int id;
+    public String original_title;
+    public String original_language;
+    public String title;
+    public String backdrop_path;
+    public Double popularity;
+    public int vote_count;
+    public Boolean video;
+    public Double vote_average;
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassigment/xirpl514/muveerku/model/SourcesResponse.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.privateassigment.xirpl514.muveerku.model;
+
+import java.util.List;
+
+/**
+ * Created by hyuam on 18/04/2017.
+ */
+
+public class SourcesResponse {
+
+    public List<Result> results;
+
+}
